0f37ed6e43d8abc869120a20f3bb795100c43bfc
Zweli <78607664+Zweli-Sa@users.noreply.github.com>
2022-02-24 13:09:37 +0200

    head

--- src/components/common/head/head.js
+++ src/components/common/head/head.js
@@ -5,7 +5,7 @@ function IndexHead() {
   return (
     <>
       <Head>
-        <link rel="icon" sizes="42x42" href="https://postimg.cc/sM31gsgn" type="image/png" />
+        <link rel="icon" sizes="32x32" href="https://i.postimg.cc/tCH2TygD/favicon.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100;200;400;600;700&display=swap" rel="stylesheet" />
