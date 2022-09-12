3e241bfb5890b480eace046fbb2f1cbdcc56db81
Zweli <78607664+Zweli-Sa@users.noreply.github.com>
2022-09-12 12:20:58 +0200

    jest.setup.js comment update

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
-// Configs e Extensões do Jest
+// Jest Configs and Extensions
 import 'jest-styled-components'; // Styled Components
 import 'jest-canvas-mock'; // <Lottie /> (JS-DOM)
-import '@testing-library/jest-dom'; // Traz expects decentes para lidar com styles e outras coisas do HTML
+import '@testing-library/jest-dom'; // Brings decent expects to handle styles and other HTML stuff
