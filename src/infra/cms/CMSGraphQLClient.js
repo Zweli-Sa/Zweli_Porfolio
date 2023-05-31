ed272437ba5082e00d83947b6a3925ff411f7d79
Zweli <78607664+Zweli-Sa@users.noreply.github.com>
2023-05-31 11:19:40 +0200

    updated DataCMSURL variable

--- src/infra/cms/CMSGraphQLClient.js
+++ src/infra/cms/CMSGraphQLClient.js
@@ -5,8 +5,8 @@ export const gql = GraphQLTag;
 export function CMSGraphQLClient() {
    const TOKEN = process.env.REACT_APP_GRAPH_CMS_DATA;
  
-  const DatoCMSURL = 'https://api-sa-east-1.graphcms.com/v2/ckzmih1w24g1001xz4kig8hk2/master';
-  const client = new GraphQLClient(DatoCMSURL, {
+  const DataCMSURL = 'https://api-sa-east-1.graphcms.com/v2/ckzmih1w24g1001xz4kig8hk2/master';
+  const client = new GraphQLClient(DataCMSURL, {
     headers: {
          Authorization: `Bearer ${TOKEN}`
     },
